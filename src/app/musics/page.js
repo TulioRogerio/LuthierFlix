df62475847e8c3ea3f534bb3abfd43df0a0202a0
Túlio Rogério <the.tulio@gmail.com>
2026-05-24 20:43:34 -0300

    feat: implement videos and musics pages and update project dependencies with security overrides

---
+++ src/app/musics/page.js
@@ -0,0 +1,8 @@
+export default function VideosPage() {
+    return (
+        <>
+            <h2>Vídeos</h2>
+            <p>Lista de vídeos</p>
+        </>
+    );
+}
---
+++ src/app/videos/page.js
@@ -0,0 +1,7 @@
+export default function VideosPage() {
+    return (
+        <div>
+            <h2>Vídeos</h2>
+        </div>
+    );
+}
